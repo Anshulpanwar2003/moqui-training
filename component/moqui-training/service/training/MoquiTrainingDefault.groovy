9edd8a63fbc742e015fd8b4bf5569633eedc7d2b
Anshul Singh panwar <104496566+Anshulpanwar2003@users.noreply.github.com>
2025-01-09 20:48:17 +0530

    Add files via upload

--- component/moqui-training/service/training/MoquiTrainingDefault.groovy
+++ component/moqui-training/service/training/MoquiTrainingDefault.groovy
@@ -1,41 +1,37 @@
 import org.moqui.context.ExecutionContext
 import org.moqui.entity.EntityValue
 
-
 ExecutionContext ec = context.ec
 
-// Input parameters
+
 def trainingName = context.trainingName
 def trainingDate = context.trainingDate
 def trainingPrice = context.trainingPrice
 def trainingDuration = context.trainingDuration
 
-// Validate required fields
+
 if (!trainingName) {
-    ec.message.addError("Training name is required.")
-    return
+    throw new Exception("Training name is required.")
+    return;
 }
-
 if (!trainingDate) {
-    ec.message.addError("Training date is required.")
-    return
+    throw new Exception("Training date is required.")
+    return;
 }
 
-// Explicitly generate a unique ID
-def trainingId = ec.entity.sequencedIdPrimary("MoquiTraining", null, null)
+def trainingId = ec.entity.sequencedIdPrimary("MoquiTraining")
 
-// Create the MoquiTraining entity record
+
 EntityValue trainingRecord = ec.entity.makeValue("moqui.training.MoquiTraining")
+trainingRecord.setAll([
+        trainingId     : trainingId,
+        trainingName   : trainingName,
+        trainingDate   : trainingDate,
+        trainingPrice  : trainingPrice ?: null,
+        trainingDuration: trainingDuration ?: null
+])
 
-trainingRecord.set("trainingId", trainingId) // Explicitly set trainingId
-trainingRecord.set("trainingName", trainingName)
-trainingRecord.set("trainingDate", trainingDate)
 
-if (trainingPrice != null) trainingRecord.set("trainingPrice", trainingPrice)
-if (trainingDuration != null) trainingRecord.set("trainingDuration", trainingDuration)
-
-// Save the record
 trainingRecord = trainingRecord.create()
 
-// Set the output parameter
-context.trainingId = trainingRecord.get("trainingId")
+context.trainingId = trainingRecord.trainingId
